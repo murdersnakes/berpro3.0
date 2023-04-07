8c0280ab2f8d38642bb4171d35723a74e5816f22
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2023-04-07 13:37:41 +0100

    form issue: unresolved

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Form from '../components/Form';
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+	return (
+		<div>
+			<div className='cont py-24'>
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+					<div>
+						<h1 className='h2'>Contact us</h1>
+					</div>
+					<Form />
+				</div>
+			</div>
+		</div>
+	);
 }
