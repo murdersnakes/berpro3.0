2ab46cea0c18ce7f260ef3dd9fbc486cc2f41494
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2023-04-09 19:25:20 +0100

    layout meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,57 @@ import '../styles/globals.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import { Archivo, Open_Sans } from 'next/font/google';
+import type { Metadata } from 'next';
 
-export const metadata = {
-	title: 'BERpro: BER Assessments & Certificates for Energy-Efficient Home',
+export const metadata: Metadata = {
+	title: 'BERpro: BER Assessments & Certificates for Energy-Efficient Homes',
 	description:
-		'BERpro is a BER assessment and certificate provider for energy-efficient homes. We provide BER assessments and certificates for homes, apartments, and commercial buildings.',
-	keywords:
-		'BER, BER assessment, BER certificate, energy efficiency, home energy rating, energy performance, DEAP software, energy assessment, BER assessor',
+		'BERpro is a BER assessment and certificate provider for energy-efficient homes in Meath, Louth & Dublin. We provide BER assessments and certificates for homes fast.',
+	keywords: [
+		'BER',
+		'BER assessment',
+		'BER certificate',
+		'energy efficiency',
+		'home energy rating',
+		'energy performance',
+		'DEAP software',
+		'energy assessment',
+		'BER assessor',
+	],
+
+	icons: {
+		icon: '/public/favicon.ico',
+		shortcut: '/public/favicon.ico',
+		apple: '/public/apple-touch-icon.png',
+	},
+	manifest: '/public/site.webmanifest',
+	generator: 'Next.js',
+	applicationName: 'BERpro',
+	referrer: 'origin-when-cross-origin',
+	viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+	authors: { name: "Michael O'Reilly" },
+	colorScheme: 'dark',
+	creator: "Michael O'Reilly",
+	publisher: "Michael O'Reilly",
+	formatDetection: {
+		email: false,
+		address: false,
+		telephone: false,
+	},
+	robots: {
+		index: false,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	themeColor: 'black',
 };
 
 const headFont = Archivo({
