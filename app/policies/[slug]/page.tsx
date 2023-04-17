f37ba06bea1d87baa002ca32399481aafcf920e1
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2023-04-17 11:23:29 +0100

    policy metadata

--- app/policies/[slug]/page.tsx
+++ app/policies/[slug]/page.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import policyData from '../../../data/policies';
+import React from "react";
+import policyData from "../../../data/policies";
+import { Metadata } from "next";
 
-export default function page({ params }) {
-	const { slug } = params;
-	const policy = policyData.find((policy) => policy.slug === slug);
-	return (
-		<div className="text-[var(--body-color)] bg-[var(--text-color)]">
-			<div className="cont py-24 ">
-				<div
-					className="prose prose-h1:h2 prose-h2:h3 prose-p:p1 mx-auto"
-					dangerouslySetInnerHTML={{ __html: policy.body }}
-				></div>
-			</div>
-		</div>
-	);
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const { slug } = params;
+  const policy = policyData.find((policy): boolean => policy.slug === slug);
+  return {
+    title: policy.title,
+  };
+}
+
+export default function page({ params }): JSX.Element {
+  const { slug } = params;
+  const policy = policyData.find((policy): boolean => policy.slug === slug);
+  return (
+    <div className='text-[var(--body-color)] bg-[var(--text-color)]'>
+      <div className='cont py-24 '>
+        <div
+          className='prose prose-h1:h2 prose-h2:h3 prose-p:p1 mx-auto'
+          dangerouslySetInnerHTML={{ __html: policy.body }}
+        ></div>
+      </div>
+    </div>
+  );
 }
